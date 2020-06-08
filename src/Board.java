f251c4826135588f3369d7fe0f1031d381414c48
TaranGilll <soccertsg@gmail.com>
2020-06-08 11:53:43 -0400

    Board

--- src/Board.java
+++ src/Board.java
@@ -1,32 +1,55 @@
-import java.util.Arrays;
+import java.awt.*;
+import javax.swing.*;
+import java.sql.*;
 
-public class Board
-{
-    private static Square[][] board;
 
-    public Board()
-    {
-        initializeBoard();
+public class Board {
+    // Declaring Variables
+    private JPanel gui = new JPanel(new BorderLayout(3, 3));
+    private JPanel chessBoard;
+    private JButton[][] chessBoardSquares = new JButton[8][8];
+
+    public Board() { initializeGui(); }
+
+    public final void initializeGui() {
+        // Initializing GUI
+        chessBoard = new JPanel(new GridLayout(0,8));
+        gui.add(chessBoard);
+
+        // Insets specifies the space that must be left at each of the square edges.
+        Insets chessSquaresMargin = new Insets(0,0,0,0);
+        for (int row = 0; row < chessBoardSquares.length; row++) {
+            for (int col = 0; col < chessBoardSquares[row].length; col++) {
+                JButton chessSquare = new JButton();
+                chessSquare.setMargin(chessSquaresMargin);
+                if ((col % 2 == 1 && row % 2 == 1) || (col % 2 == 0 && row % 2 == 0)) {
+                    chessSquare.setBackground(Color.WHITE);
+                    chessSquare.setOpaque(true);
+                }
+                else {
+                    chessSquare.setBackground(Color.BLACK);
+                    chessSquare.setOpaque(true);
+                }
+                chessBoardSquares[row][col] = chessSquare;
+                chessBoard.add(chessBoardSquares[row][col]);
+            }
+
+        }
     }
 
-    public void initializeBoard()
-    {
-        //Test run to set the board with all the Pawns;
-        board = new Square[8][8];
-        for (int i = 0; i < board.length; i++)
-        {
-            board [1][i] = new Square (1, i ,new Pawn(true) );
-        }
-        for (int i = 0; i < board.length; i++)
-        {
-            board [6][i] = new Square (6, i ,new Pawn(false) );
-        }
-
+    public final JComponent getGui() {
+        return gui;
     }
 
-    public static void main (String[] args)
-    {
-        Board b1 = new Board();
-        System.out.println(Arrays.deepToString(board));
+    public static void main(String[] args) {
+
+        Board cb = new Board();
+        JFrame frame = new JFrame("Chess");
+        frame.add(cb.getGui());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        frame.setMinimumSize(frame.getSize());
+        frame.setVisible(true);
     }
 }
