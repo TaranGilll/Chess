206da2d836fe10f8465e600c588799ac50169401
TaranGilll <soccertsg@gmail.com>
2020-06-11 13:52:44 -0400

    Board with pieces

---
+++ src/Move.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Move extends Board {
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == chessBoardSquares[1][0]) {
+            ImageIcon icon = new ImageIcon();
+            icon = (ImageIcon) chessBoardSquares[1][0].getIcon();
+            chessBoardSquares[3][0].setIcon(icon);
+        }
+    }
+
+    public static void main(String[] args) {
+        //
+    }
+}
